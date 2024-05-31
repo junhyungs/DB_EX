c58ef4a81191bd3a9d86170383e8efd3126e6633
junhyungs <jin05003@naver.com>
2024-05-31 14:19:54 +0900

    [Update]DB_Select,Insert기능 추가

--- Assets/Mirror/Ref/MirrorChat_Danitech/Scripts/DatabaseUI.cs
+++ Assets/Mirror/Ref/MirrorChat_Danitech/Scripts/DatabaseUI.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using MySql.Data.MySqlClient;
 using System;
+using System.Data;
 
 public class DatabaseUI : MonoBehaviour
 {
@@ -16,9 +17,88 @@ public class DatabaseUI : MonoBehaviour
     [SerializeField] string m_UId = "root";
     [SerializeField] string m_Password = "1234";
 
-    private bool m_isConnectTestComplete; //중요하지 않음음
+    private bool m_isConnectTestComplete; //중요하지 않음
 
     private static MySqlConnection m_DbConnection;
+
+    private void Awake()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    private void SendQuery(string queryStr,string tableName)
+    {
+        //있으면 Select 관련 함수 호출
+        if (queryStr.Contains("SELECT"))
+        {
+            DataSet dataSet = OnSelectRequest(queryStr, tableName);
+            Text_DBResult.text = DeformatResult(dataSet);
+        }
+        else //없다면 Insert 또는 Update 관련 쿼리
+        {
+            Text_DBResult.text = OnInsertOnUpdateRequest(queryStr) ? "성공" : "실패";
+        }
+    }
+    //sql 관련 함수로 사용하려면 static 함수로 정의해야함.
+    public static bool OnInsertOnUpdateRequest(string query)
+    {
+        try
+        {
+            MySqlCommand sqlCommand = new MySqlCommand();
+            sqlCommand.Connection = m_DbConnection;
+            sqlCommand.CommandText = query;
+
+            m_DbConnection.Open();
+            sqlCommand.ExecuteNonQuery();
+            m_DbConnection.Close();
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    private string DeformatResult(DataSet dataSet)
+    {
+        string resultStr = string.Empty;
+
+        foreach(DataTable table in dataSet.Tables)
+        {
+            foreach(DataRow row in table.Rows)
+            {
+                foreach(DataColumn column in table.Columns)
+                {
+                    resultStr += $"{column.ColumnName}: {row[column]}\n";
+                }
+            }
+        }
+
+        return resultStr;
+    }
+
+    public static DataSet OnSelectRequest(string query, string tableName)
+    {
+        try
+        {
+            m_DbConnection.Open();
+            MySqlCommand sqlCmd = new MySqlCommand();
+            sqlCmd.Connection = m_DbConnection;
+            sqlCmd.CommandText = query;
+
+            MySqlDataAdapter sd = new MySqlDataAdapter(sqlCmd);
+            DataSet dataSet = new DataSet();
+            sd.Fill(dataSet, tableName);
+
+            m_DbConnection.Close();
+            return dataSet;
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.ToString());
+            return null;    
+        }
+    }
 
     private bool ConnectTest()
     {
@@ -50,6 +130,17 @@ public class DatabaseUI : MonoBehaviour
 
     public void OnSubmit_SendQuery()
     {
+        if(m_isConnectTestComplete == false)
+        {
+            Text_Log.text = "DB 연결을 먼저 시도하세요";
+            return;
+        }
+        Text_Log.text = string.Empty;
+
+        string query = string.IsNullOrWhiteSpace(Input_Query.text) ? "SELECT U_Name,U_Password FROM user_info"
+            : Input_Query.text;
+
+        SendQuery(query, "user_info");
     }
 
     public void OnClick_OpenDatabaseUI()
